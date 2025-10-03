17444804a298fbae0d1e895499a649ee9872e60f
mike <59279940+mikki-design@users.noreply.github.com>
2025-10-03 05:23:57 -0700

    Update useAddComment.ts

--- src/lib/react-query/mutations/useAddComment.ts
+++ src/lib/react-query/mutations/useAddComment.ts
@@ -1,6 +1,6 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { databases } from "@/lib/appwrite/config";
-import { ID, Permission, Role } from "appwrite";
+import { ID } from "appwrite";
 import { appwriteConfig } from "@/lib/appwrite/config";
 
 const DATABASE_ID = appwriteConfig.databaseId;
