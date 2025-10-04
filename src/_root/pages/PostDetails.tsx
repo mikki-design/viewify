4c9b65b75085c5153c4b37e396e413cfae5e7b3a
mike <59279940+mikki-design@users.noreply.github.com>
2025-10-03 17:30:13 -0700

    Update PostDetails.tsx

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -292,7 +292,7 @@ const PostDetails = () => {
                   type="submit"
                   
                   disabled={!newComment.trim() || isAddingComment}
-                  onClick={handleAddComment}
+                  
                 >
               
                   Post
