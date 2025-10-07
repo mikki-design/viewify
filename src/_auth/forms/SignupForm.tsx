3d6cae993856b7a1e2751ac9f1f6e359e0f02c9b
mike <59279940+mikki-design@users.noreply.github.com>
2025-10-07 15:16:28 -0700

    Update SignupForm.tsx

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -24,7 +24,7 @@ import { SignupValidation } from "@/lib/validation";
 import { useUserContext } from "@/context/AuthContext";
 
 const SignupForm = () => {
-  const { toast } = useToast();
+  
   const navigate = useNavigate();
   const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
 
